0d4e180d18bc61977fe7c098567e58cb9b156b05
Mubin <mubinnoma@gmail.com>
2025-03-18 19:40:59 +0600

    Updated DAL to use stored procedures. Get all and get by id implemented.

--- Repositories/ProductRepository.cs
+++ Repositories/ProductRepository.cs
@@ -21,19 +21,24 @@ namespace InventoryManagement.Repositories
 
         public async Task<List<Product>> GetAllAsync()
         {
-            var products = await _context.product.ToListAsync();
+            //var products = await _context.product.ToListAsync(); // Direct Fetch
+            //return products;
+
+            var products = await _context.product.FromSqlRaw("GetAllProducts").ToListAsync(); // Stored Procedure Fetch
             return products;
         }
 
         public async Task<Product?> GetByIDAsync(int id)
         {
-            var product = await _context.product.FirstOrDefaultAsync(m => m.Id == id);
-            return product;
+            //var product = await _context.product.FirstOrDefaultAsync(m => m.Id == id); // Direct Fetch
+            //return product;
+
+            var product = await _context.product.FromSqlInterpolated($"EXEC GetById {id}").ToListAsync(); // Stored Procedure Fetch
+            return product.FirstOrDefault(); // Converting List to Product model instance
         }
 
         public void Insert(Product product)
         {
-            
             _context.Add(product);
         }
 
@@ -44,12 +49,12 @@ namespace InventoryManagement.Repositories
 
         public bool ProductExists(int id)
         {
-            return _context.product.Any(e => e.Id == id);
+            return _context.product.Any(e => e.Id == id); // Direct Fetch
         }
 
         public IEnumerable<Product> GetAll()
         {
-            return _context.product;
+            return _context.product.FromSqlRaw("GetAllProducts"); // Stored Procedure Fetch
         }
     }
 }
